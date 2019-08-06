0d34e5b6c065c41a2946d862011b91292bf8c572
すー <aaaiiiuuu35@gmail.com>
2019-08-06 20:33:47 +0900

    1

--- p_001 Two Sum/p_001 Two Sum/Program.cs
+++ p_001 Two Sum/p_001 Two Sum/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace p_001_Two_Sum
 {
@@ -6,10 +8,12 @@ namespace p_001_Two_Sum
     {
         public static void Main(string[] args)
         {
+            var n = new Solution().TwoSum(new []{2,7,11,15}, 9);
+            Console.WriteLine($"{n?[0]} {n?[1]}");
         }
     }
     
-    public class Solution {
+    public class Solution1 {
         public int[] TwoSum(int[] nums, int target) {
             for (int i = 0; i < nums.Length; i++)
             {
@@ -19,6 +23,22 @@ namespace p_001_Two_Sum
                 }
             }
             throw new Exception("not s");
+        }
+    }
+    
+    public class Solution {
+        public int[] TwoSum(int[] nums, int target)
+        {
+            var dict = Enumerable.Range(0, nums.Length).ToDictionary(i => nums[i].GetHashCode());
+            for (var i = 0; i < nums.Length; i++)
+            {
+                var search = target - nums[i];
+                if (dict.ContainsKey(search.GetHashCode()) && dict[search.GetHashCode()] != i)
+                {
+                    return new[] {i, dict[search.GetHashCode()]};
+                }
+            }
+            throw new ArgumentException("No two sum solution");
         }
     }
 }
