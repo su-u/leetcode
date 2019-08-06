cd14c75412bda37945eea30a0a6e4d34738622c6
すー <aaaiiiuuu35@gmail.com>
2019-08-06 19:13:00 +0900

    1

--- p_001 Two Sum/p_001 Two Sum/Program.cs
+++ p_001 Two Sum/p_001 Two Sum/Program.cs
@@ -1,9 +1,24 @@
-﻿namespace p_001_Two_Sum
+﻿using System;
+
+namespace p_001_Two_Sum
 {
     internal class Program
     {
         public static void Main(string[] args)
         {
+        }
+    }
+    
+    public class Solution {
+        public int[] TwoSum(int[] nums, int target) {
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = i + 1; j < nums.Length; j++)
+                {
+                    if(nums[i] + nums[j] == target) return new Int32[]{i,j};
+                }
+            }
+            throw new Exception("not s");
         }
     }
 }
